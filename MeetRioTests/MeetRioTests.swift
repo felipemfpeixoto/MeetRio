f666f5e8f2de088aa82780d423bf6522338fff11
Felipe Peixoto <felipe.m.f.peixoto@gmail.com>
2024-09-27 14:53:34 -0300

    Initial Commit

---
+++ MeetRioTests/MeetRioTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeetRioTests.swift
+//  MeetRioTests
+//
+//  Created by Felipe on 27/09/24.
+//
+
+import Testing
+@testable import MeetRio
+
+struct MeetRioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
